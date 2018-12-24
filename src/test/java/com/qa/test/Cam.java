54ee0f13652177f22231b1c24372d403b8e3cdeb
k2annex <k2annex@k2annex03>
2018-12-24 12:00:19 +0530

    New code for demo

---
+++ src/test/java/com/qa/test/Cam.java
@@ -0,0 +1,40 @@
+package com.qa.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Cam {
+	
+	WebDriver driver;
+	  @Test
+	  public void Login() {
+		  
+		 driver.findElement(By.xpath("//a[@title='My Account']")).click();
+		  driver.findElement(By.xpath("//input[@id='log']")).sendKeys("test");
+	      driver.findElement(By.xpath("//input[@id='pwd']")).sendKeys("password");
+	      driver.findElement(By.xpath("//input[@id='login']")).click();
+	  }
+	  @BeforeClass
+	  public void beforeClass() {
+		  
+		  System.setProperty("webdriver.chorme.driver", "D:/Pritam/CD/chromedriver");
+		  driver = new ChromeDriver();
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  driver.manage().window().maximize();
+		  driver.get("http://store.demoqa.com/");
+	  }
+
+	  @AfterClass
+	  public void Teardown() {
+		  
+		  driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		  driver.quit();
+	  }
+
+}
